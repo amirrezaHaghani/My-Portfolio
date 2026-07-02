237fa92af34e879b756bc7c2457ac6dce44cd653
Amirreza <Amirrezahaghani96@gmail.com>
2026-07-02 11:54:14 +0330

    Randomize Android mascot animations

--- src/components/AndroidMascot.vue
+++ src/components/AndroidMascot.vue
@@ -1,10 +1,16 @@
+<script setup lang="ts">
+const mascotStyles = ['hello', 'wink', 'nod', 'spark', 'peek'] as const;
+const mascotStyle = mascotStyles[Math.floor(Math.random() * mascotStyles.length)];
+</script>
+
 <template>
-  <span class="android-mascot" aria-hidden="true">
+  <span class="android-mascot" :class="`android-mascot--${mascotStyle}`" aria-hidden="true">
     <span class="android-antenna android-antenna-left"></span>
     <span class="android-antenna android-antenna-right"></span>
     <span class="android-head">
       <span class="android-eye"></span>
       <span class="android-eye"></span>
+      <span class="android-mouth"></span>
     </span>
     <span class="android-body">
       <span class="android-arm android-arm-left"></span>
@@ -12,5 +18,7 @@
       <span class="android-leg android-leg-left"></span>
       <span class="android-leg android-leg-right"></span>
     </span>
+    <span class="android-spark android-spark-one"></span>
+    <span class="android-spark android-spark-two"></span>
   </span>
 </template>
